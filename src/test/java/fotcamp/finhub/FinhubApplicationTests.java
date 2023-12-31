ca45f1ebd70655c7b906844c98dbf5307ca5e504
M1nseokSong <minseok3206@gmail.com>
2024-01-01 02:33:39 +0900

    :art: Finhub project start !

---
+++ src/test/java/fotcamp/finhub/FinhubApplicationTests.java
@@ -0,0 +1,13 @@
+package fotcamp.finhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
