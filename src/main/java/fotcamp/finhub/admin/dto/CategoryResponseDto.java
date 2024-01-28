1b49f6d5575d65070afacf6fd17fee531acf23bf
M1nseokSong <minseok3206@gmail.com>
2024-01-28 14:04:57 +0900

    :white_check_mark: 카테고리 상세조회 api 썸네일 추가

--- src/main/java/fotcamp/finhub/admin/dto/CategoryResponseDto.java
+++ src/main/java/fotcamp/finhub/admin/dto/CategoryResponseDto.java
@@ -7,11 +7,13 @@ import lombok.Getter;
 public class CategoryResponseDto {
     private final Long id;
     private final String name;
+    private final String thumbnailImgPath;
     private final String useYN;
 
     public CategoryResponseDto(Category category) {
         this.id = category.getId();
         this.name = category.getName();
+        this.thumbnailImgPath = category.getThumbnailImgPath();
         this.useYN = category.getUseYN();
     }
 }
--- src/main/java/fotcamp/finhub/admin/dto/DetailCategoryResponseDto.java
+++ src/main/java/fotcamp/finhub/admin/dto/DetailCategoryResponseDto.java
@@ -3,21 +3,21 @@ package fotcamp.finhub.admin.dto;
 import fotcamp.finhub.common.domain.Category;
 import lombok.Getter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 public class DetailCategoryResponseDto {
-    private Long categoryId;
-    private String name; // 카테고리명
-    private String useYN; // 카테고리 useYN
-    private List<DetailCategoryTopicResponseDto> topicList = new ArrayList<>();
+    private final Long categoryId;
+    private final String name; // 카테고리명
+    private final String thumbnailImgPath; // 카테고리 썸네일 경로
+    private final String useYN; // 카테고리 useYN
+    private final List<DetailCategoryTopicResponseDto> topicList;
 
     public DetailCategoryResponseDto(Category category, List<DetailCategoryTopicResponseDto> dtoList) {
         this.categoryId = category.getId();
         this.name = category.getName();
+        this.thumbnailImgPath = category.getThumbnailImgPath();
         this.useYN = category.getUseYN();
         this.topicList = dtoList;
     }
-
 }
