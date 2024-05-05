b22e2f7f45549b672ec046bc17c87b372144bac7
hyeon-gyu <hyungue729@naver.com>
2024-05-05 17:35:41 +0900

    #158 ADMIN 공지사항 상세페이지

--- src/main/java/fotcamp/finhub/main/repository/MemberRepository.java
+++ src/main/java/fotcamp/finhub/main/repository/MemberRepository.java
@@ -2,7 +2,10 @@ package fotcamp.finhub.main.repository;
 
 import fotcamp.finhub.common.domain.Member;
 import fotcamp.finhub.common.domain.Topic;
+import fotcamp.finhub.common.domain.UserType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,5 +22,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 
     List<Member> findByPushYn(boolean pushYn);
+
+    @Query("SELECT COUNT(m) FROM Member m WHERE m.userType = :usertype")
+    long countMemberUsingUsertype(@Param("usertype") UserType userType);
 
 }
