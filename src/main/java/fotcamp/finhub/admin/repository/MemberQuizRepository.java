699cf382e66ae01e10888eea437dad739b7778b6
hyeon-gyu <hyungue729@naver.com>
2024-05-08 11:14:12 +0900

    #179 admin 컬럼, 퀴즈 삭제 api

--- src/main/java/fotcamp/finhub/admin/repository/MemberQuizRepository.java
+++ src/main/java/fotcamp/finhub/admin/repository/MemberQuizRepository.java
@@ -29,4 +29,5 @@ public interface MemberQuizRepository extends JpaRepository<MemberQuiz, Long> {
     @Query("SELECT new fotcamp.finhub.main.dto.response.quiz.QuizInfoDto(q.id, q.question, q.targetDate) FROM MemberQuiz mq JOIN mq.quiz q WHERE mq.member.memberId = :memberId AND q.targetDate < :cursorDate ORDER BY q.targetDate DESC")
     List<QuizInfoDto> findSolvedQuizInfoByMemberId(@Param("memberId") Long memberId, @Param("cursorDate") LocalDate cursorDate, Pageable pageable);
 
+    List<MemberQuiz> findByQuiz(Quiz quiz);
 }
