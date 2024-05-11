4339417c5f7d152d7c6081aedfd959bbb11e48f5
M1nseokSong <minseok3206@gmail.com>
2024-05-11 13:34:38 +0900

    :white_check_mark: admin 컬럼 댓글 관리 api 수정

--- src/main/java/fotcamp/finhub/main/dto/process/ReportedCommentsProcessDto.java
+++ src/main/java/fotcamp/finhub/main/dto/process/ReportedCommentsProcessDto.java
@@ -9,16 +9,20 @@ import java.time.LocalDateTime;
 @Getter
 public class ReportedCommentsProcessDto {
     private final Long id;
+    private final Long commentId;
     private final String comment;
     private final String reason;
     private final String useYn;
-    private final String writerNickname;
+    private final String reportedNickname;
+    private final String reporterNickname;
 
     public ReportedCommentsProcessDto(CommentsReport commentsReport) {
         this.id = commentsReport.getId();
+        this.commentId = commentsReport.getReportedComment().getId();
         this.comment = commentsReport.getReportedComment().getContent();
         this.reason = commentsReport.getReportReasons().getReason();
         this.useYn = commentsReport.getReportedComment().getUseYn();
-        this.writerNickname = commentsReport.getReportedMember().getNickname();
+        this.reportedNickname = commentsReport.getReportedMember().getNickname();
+        this.reporterNickname = commentsReport.getReporterMember().getNickname();
     }
 }
