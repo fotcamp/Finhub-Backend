3730d7007f9ec72948be806b1faf48c138e47acb
hyeon-gyu <hyungue729@naver.com>
2024-05-15 19:27:44 +0900

    #190 CORS PATCH 메서드 추가

--- src/main/java/fotcamp/finhub/common/config/WebConfig.java
+++ src/main/java/fotcamp/finhub/common/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
         // Main 애플리케이션을 위한 CORS 설정
         registry.addMapping("/api/v1/main/**")
                 .allowedOrigins(adminOrigin, mainOrigin, devOrigin, "http://127.0.0.1:3000", "http://localhost:3000", "https://api.fin-hub.co.kr")  // Main 프론트엔드 애플리케이션의 도메인
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
 
