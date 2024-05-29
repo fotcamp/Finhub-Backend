69299d7bc9f9ae192bd0c464c856a7aefef6c626
M1nseokSong <minseok3206@gmail.com>
2024-05-29 20:23:46 +0900

    :white_check_mark: 인기검색어 최신날짜 데이터만 가져오기

--- src/main/java/fotcamp/finhub/main/service/MainService.java
+++ src/main/java/fotcamp/finhub/main/service/MainService.java
@@ -272,7 +272,7 @@ public class MainService {
     public ResponseEntity<ApiResponseWrapper> popularKeyword(){
         int rank = 1;
         List<PopularSearchDto> popularSearchDtoList = new ArrayList<>();
-        List<WeekPopularSearch> weekPopularSearchList = weekPopularKeywordRepository.findAllByOrderByIdAsc();
+        List<WeekPopularSearch> weekPopularSearchList = weekPopularKeywordRepository.findTopByOrderByAnalysisDateDesc();
         for (WeekPopularSearch weekPopularSearch : weekPopularSearchList) {
             popularSearchDtoList.add(new PopularSearchDto(rank, weekPopularSearch.getKeyword(), weekPopularSearch.getTrend()));
             rank += 1;
