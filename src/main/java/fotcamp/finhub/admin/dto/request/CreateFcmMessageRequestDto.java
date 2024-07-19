1df6b7ac465533515f639d837391b62aba05799c
hyeon-gyu <hyungue729@naver.com>
2024-07-19 15:58:18 +0900

    #247 FCM 푸시 API 요청, 응답 포맷 변경, 코드 리팩토링

--- src/main/java/fotcamp/finhub/admin/dto/request/CreateFcmMessageRequestDto.java
+++ src/main/java/fotcamp/finhub/admin/dto/request/CreateFcmMessageRequestDto.java
@@ -6,25 +6,27 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 public class CreateFcmMessageRequestDto {
 
-    private String target; // admin, all, email
+    private Long type; // 0 ~ 4번
+    private List<String> target; // 이메일 리스트, 혹은 빈 리스트
     private String title;
     private String content;
     private String view;
     private FcmMessageProcessDto.Action action;
-    private Long mutableContent;
 
     @Builder
-    public CreateFcmMessageRequestDto(String target, String title, String content, String view, FcmMessageProcessDto.Action action, Long mutableContent) {
+    public CreateFcmMessageRequestDto(Long type, List<String> target, String title, String content, String view, FcmMessageProcessDto.Action action) {
+        this.type = type;
         this.target = target;
         this.title = title;
         this.content = content;
         this.view = view;
         this.action = action;
-        this.mutableContent = mutableContent;
     }
 
 }
---
+++ src/main/java/fotcamp/finhub/admin/dto/response/FcmResponseDto.java
@@ -0,0 +1,6 @@
+package fotcamp.finhub.admin.dto.response;
+
+import java.util.List;
+
+public record FcmResponseDto(List<String> failList) {
+}
