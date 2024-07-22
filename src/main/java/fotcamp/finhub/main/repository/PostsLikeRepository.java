160e63528635c6a56329c82778893a7d344a880c
hyeon-gyu <hyungue729@naver.com>
2024-07-22 13:56:56 +0900

    #241 회원탈퇴하기 api 디버깅 (컬럼 좋아요 레코드 삭제)

--- src/main/java/fotcamp/finhub/main/repository/PostsLikeRepository.java
+++ src/main/java/fotcamp/finhub/main/repository/PostsLikeRepository.java
@@ -3,14 +3,17 @@ package fotcamp.finhub.main.repository;
 import fotcamp.finhub.common.domain.GptColumn;
 import fotcamp.finhub.common.domain.Member;
 import fotcamp.finhub.common.domain.PostsLike;
+import fotcamp.finhub.common.domain.PostsScrap;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PostsLikeRepository extends JpaRepository<PostsLike, Long> {
     Optional<PostsLike> findFirstByGptColumnAndMember(GptColumn gptColumn, Member member);
     Long countByGptColumn(GptColumn gptColumn);
+    List<PostsLike> findByMember(Member member);
 
 }
