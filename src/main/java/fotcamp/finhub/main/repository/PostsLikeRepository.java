8c11ee8426297e04b5aaf8cfc1f6fcf7abc3479e
hyeon-gyu <hyungue729@naver.com>
2024-07-22 14:00:09 +0900

    #241 회원탈퇴하기 api 디버깅 - 수정 (컬럼 좋아요 레코드 삭제)

--- src/main/java/fotcamp/finhub/main/repository/PostsLikeRepository.java
+++ src/main/java/fotcamp/finhub/main/repository/PostsLikeRepository.java
@@ -3,7 +3,6 @@ package fotcamp.finhub.main.repository;
 import fotcamp.finhub.common.domain.GptColumn;
 import fotcamp.finhub.common.domain.Member;
 import fotcamp.finhub.common.domain.PostsLike;
-import fotcamp.finhub.common.domain.PostsScrap;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
