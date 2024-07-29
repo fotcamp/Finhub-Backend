a8b04da54f1c91fab51fe5aa54a7263bab6dbdf1
M1nseokSong <minseok3206@gmail.com>
2024-07-29 23:09:47 +0900

    :white_check_mark: Webconfig cors 경로 yml 관리

--- src/main/java/fotcamp/finhub/common/config/WebConfig.java
+++ src/main/java/fotcamp/finhub/common/config/WebConfig.java
@@ -11,27 +11,31 @@ public class WebConfig implements WebMvcConfigurer {
     @Value("${origin.admin}") String adminOrigin;
     @Value("${origin.main}") String mainOrigin;
     @Value("${origin.dev}") String devOrigin;
+    @Value("${origin.dev}") String localOne;
+    @Value("${origin.dev}") String localTwo;
+    @Value("${origin.dev}") String localThree;
+    @Value("${origin.dev}") String serverSwagger;
 
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // Admin 애플리케이션을 위한 CORS 설정
         registry.addMapping("/api/v1/admin/**")
-                .allowedOrigins(mainOrigin, adminOrigin, devOrigin, "http://127.0.0.1:3000", "http://localhost:3000","http://127.0.0.1:3001", "http://localhost:3001", "https://api.fin-hub.co.kr")  // Admin 프론트엔드 애플리케이션의 도메인
+                .allowedOrigins(mainOrigin, adminOrigin, devOrigin, localOne, localTwo, localThree, serverSwagger)  // Admin 프론트엔드 애플리케이션의 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
 
         // Main 애플리케이션을 위한 CORS 설정
         registry.addMapping("/api/v1/main/**")
-                .allowedOrigins(adminOrigin, mainOrigin, devOrigin, "http://127.0.0.1:3000", "http://localhost:3000" ,"http://127.0.0.1:3001", "http://localhost:3001", "https://api.fin-hub.co.kr")  // Main 프론트엔드 애플리케이션의 도메인
+                .allowedOrigins(adminOrigin, mainOrigin, devOrigin, localOne, localTwo, localThree, serverSwagger)  // Main 프론트엔드 애플리케이션의 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
 
         // Auth 애플리케이션을 위한 CORS 설정
         registry.addMapping("/api/v1/auth/**")
-                .allowedOrigins(adminOrigin, mainOrigin, devOrigin, "http://127.0.0.1:3000", "http://localhost:3000","http://127.0.0.1:3001", "http://localhost:3001", "https://api.fin-hub.co.kr")  // Main 프론트엔드 애플리케이션의 도메인
+                .allowedOrigins(adminOrigin, mainOrigin, devOrigin, localOne, localTwo, localThree, serverSwagger)  // Main 프론트엔드 애플리케이션의 도메인
                 .allowedMethods("GET", "POST", "PUT")
                 .allowedHeaders("*")
                 .allowCredentials(true);
