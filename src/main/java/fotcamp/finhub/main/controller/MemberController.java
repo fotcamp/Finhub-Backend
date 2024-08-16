04e05fbc4d64b1f3f4306c1365ac71704668d28a
skysamer <smtkdals94@gmail.com>
2024-08-16 20:23:16 +0900

    #140 BE) Oauth 이메일 변경 관련 이메일 사용 화면 로직 변경
    - 유저의 이메일정보를 설정하는 신규 API 추가

--- src/main/java/fotcamp/finhub/main/controller/MemberController.java
+++ src/main/java/fotcamp/finhub/main/controller/MemberController.java
@@ -7,7 +7,10 @@ import fotcamp.finhub.main.dto.request.EmailUpdateDto;
 import fotcamp.finhub.main.dto.request.ScrapRequestDto;
 import fotcamp.finhub.main.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -24,8 +27,12 @@ public class MemberController {
     @PatchMapping
     @PreAuthorize("hasRole('USER')")
     @Operation(summary = "유저 이메일정보 설정", description = "유저의 이메일정보를 설정 및 변경한다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "변경성공"),
+            @ApiResponse(responseCode = "400", description = "이메일 형식이 올바르지 않을때(공백, 이메일패턴이아님)")
+    })
     public ResponseEntity<ApiResponseWrapper> modifyEmail(@AuthenticationPrincipal CustomUserDetails userDetails,
-            @RequestBody EmailUpdateDto emailDto){
+            @Valid @RequestBody EmailUpdateDto emailDto){
         memberService.modifyEmail(emailDto.email(), userDetails.getMemberIdasLong());
         return null;
     }
